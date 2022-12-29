c8fbcbbece32e9f829be28e9e64900a599d54da7
usmanferoz <usman_feroz786@hotmail.com>
2022-12-30 01:52:12 +0500

    fix

--- job_protal/serializer.py
+++ job_protal/serializer.py
@@ -24,16 +24,3 @@ class OrderSerializer(ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
-
-    def to_representation(self, instance):
-        data = {
-            "id" : instance.id,
-            "order_status" : instance.order_status,
-            "order_location" : instance.order_location,
-            "order_contact" : instance.order_contact,
-            "job_name" : instance.job.job_name,
-            "job_fair" : instance.job_fair,
-            "job_id" : instance.job.id
-        }
-
-        return data
